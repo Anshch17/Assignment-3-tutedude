00f297fc30b8084d84a66a4f5fb8badb3ca4583c
Anshch17 <ansh.chitransh09@gmail.com>
2025-09-13 13:12:52 +0530

    Add files via upload

---
+++ math_operations.py
@@ -0,0 +1,11 @@
+import math
+
+a = float(input("Enter a number: "))
+
+squareroot = math.sqrt(a)
+log = math.log(a)
+sine = math.sin(a)
+
+print("Square root:", squareroot)
+print("Logarithm:", log)
+print("Sine:", sine)
